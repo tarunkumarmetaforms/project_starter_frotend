40c3cbc2a0dad0c95308fede087f3a2a972ca93d
Tarun Kumar <tarunkumar@192.168.1.2>
2025-10-25 20:35:14 +0530

    fix : added the inital styles for the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { RouterProvider,  createRouter } from '@tanstack/react-router'
 import {routeTree } from './routeTree.gen'
-
 const router = createRouter({ routeTree })
 
 
